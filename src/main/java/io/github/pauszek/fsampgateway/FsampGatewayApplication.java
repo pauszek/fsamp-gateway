3ee9ed35b3193ebdaf5ad15e523d943320621d16
pauszek <senderpl12@gmail.com>
2026-01-01 00:29:16 +0100

    Initial commit

---
+++ src/main/java/io/github/pauszek/fsampgateway/FsampGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.github.pauszek.fsampgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsampGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsampGatewayApplication.class, args);
+	}
+
+}
